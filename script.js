d37f2073b9f5d1826c552a2d65d574d6e3128f91
rosalina hundahl <rofr0001@stud.kea.dk>
2025-08-25 14:53:40 +0200

    oevelse 1: tilfældigt nummer

---
+++ script.js
@@ -0,0 +1,6 @@
+let number = Math.floor(Math.random() * 100);
+
+console.log("random number " + number);
+
+const randomNumber = document.getElementById("random-number");
+randomNumber.innerText = number;
