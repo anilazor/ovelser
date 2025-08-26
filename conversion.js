44daccc8c523e6a43074bd09478fa33f0811c9ea
rosalina hundahl <rofr0001@stud.kea.dk>
2025-08-26 12:48:11 +0200

    Gæt et tal

---
+++ conversion.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const n1 = 1;
+const n2 = 2;
+const s1 = "1";
+const s2 = "2";
+
+n1 + n2;
+s1 + s2;
+n1 + s2;
+s1 + n2;
+
+const value = true;
+
+if (value) {
+  console.log("value is truthy");
+} else {
+  console.log("value is falsy");
+}
--- script.js
+++ script.js
@@ -1,6 +1,40 @@
+"use strict";
+
 let number = Math.floor(Math.random() * 100);
 
-console.log("random number " + number);
+console.log("random number: " + number);
 
 const randomNumber = document.getElementById("random-number");
 randomNumber.innerText = number;
+
+const svar = document.getElementById("svar");
+const submit = document.getElementById("submitButton").addEventListener("click", myFunction);
+
+let otherNumber = Math.floor(Math.random() * 100);
+console.log("random number 2: " + otherNumber);
+
+const popUp = document.getElementById("result");
+
+const cat = document.getElementById("cat");
+const wow = document.getElementById("wow");
+
+function myFunction() {
+  popUp.classList.remove("hidden");
+  popUp.classList.add("visible");
+
+  if (Number(svar.value) === otherNumber) {
+    popUp.innerText = " Yeyyy! <3 Det er korrekt B)";
+    cat.classList.remove("hidden");
+    cat.classList.add("visible");
+    wow.classList.remove("hidden");
+    wow.classList.add("visible");
+  }
+
+  if (Number(svar.value) < otherNumber) {
+    popUp.innerText = " Miv... </3 Tallet var for lavt :(";
+  }
+
+  if (Number(svar.value) > otherNumber) {
+    popUp.innerText = " Miv... </3 Tallet var for højt :(";
+  }
+}
