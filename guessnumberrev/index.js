1c333a42071961df0ee3e58fb26b44b61ce58cec
rosalina hundahl <rofr0001@stud.kea.dk>
2025-08-28 10:33:15 +0200

    conversion

---
+++ guessnumberrev/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const btn3 = document.querySelector("#btn3");
+
+let computerGuess = document.querySelector("#computer-guess");
