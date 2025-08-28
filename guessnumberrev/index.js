aa1b455a614f3141fd0144cd5683b103d14473ff
rosalina hundahl <rofr0001@stud.kea.dk>
2025-08-28 15:34:05 +0200

    cute stuff

--- guessnumberrev/index.js
+++ guessnumberrev/index.js
@@ -4,6 +4,7 @@ const btn1 = document.querySelector("#btn1");
 const btn2 = document.querySelector("#btn2");
 const btn3 = document.querySelector("#btn3");
 const sucess = document.querySelector("#sucess");
+const loader = document.querySelector("loader");
 
 btnStart.addEventListener("click", startSpil);
 btn1.addEventListener("click", forLav);
@@ -12,7 +13,7 @@ btn3.addEventListener("click", rigtig);
 
 let lowNum = 0;
 let highNum = 100;
-const computerGuess = document.querySelector("#computer-guess");
+const computerGuess = document.querySelector("#computer-guess h3");
 
 let guess;
 
@@ -21,15 +22,23 @@ function startSpil() {
   highNum = 100;
   makeComputerGuess();
   succes.classList.add("hidden");
+  computerGuess.classList.remove("glow");
+  computerGuess.classList.remove("scale-up");
   btn1.classList.remove("hidden");
   btn2.classList.remove("hidden");
   btn3.classList.remove("hidden");
+  computerGuess.classList.remove("fade-in"); // Reset animation
+  void computerGuess.offsetWidth; // Force reflow
+  computerGuess.classList.add("fade-in");
 }
-
+// ...existing code...
 function makeComputerGuess() {
   guess = Math.round((highNum + lowNum) / 2);
   console.log(guess);
   computerGuess.innerText = guess;
+  computerGuess.classList.remove("fade-in"); // Reset animation
+  void computerGuess.offsetWidth; // Force reflow
+  computerGuess.classList.add("fade-in");
 }
 
 function forLav() {
@@ -43,8 +52,10 @@ function forHoej() {
 }
 
 function rigtig() {
-  succes.innerText = "Succes!";
+  succes.innerText = "Succes!(ෆ˙ᵕ˙ෆ)♡";
   succes.classList.remove("hidden");
+  computerGuess.classList.add("glow");
+  computerGuess.classList.add("scale-up");
   btn1.classList.add("hidden");
   btn2.classList.add("hidden");
   btn3.classList.add("hidden");
