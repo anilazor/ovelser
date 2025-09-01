7b018da7476a30569642b8e11c9a018ceaaf9549
rosalina hundahl <rofr0001@stud.kea.dk>
2025-09-01 15:16:56 +0200

    bar chart

---
+++ bar_chart_starter_kit/script.js
@@ -0,0 +1,21 @@
+let bars = [];
+
+const list = document.querySelector("ul");
+
+setInterval(generateBar, 1000);
+
+function generateBar() {
+  console.log("generateBar");
+  const li = document.createElement("li");
+  let randoNum = Math.floor(Math.random() * 100);
+  bars.push(randoNum);
+  let barLength = bars.length;
+  console.log(bars.length);
+  console.log(bars);
+  li.style.setProperty("--height", `${randoNum}`);
+  list.appendChild(li);
+  if (barLength == 20) {
+    bars.shift();
+    document.querySelector("li").remove();
+  }
+}
